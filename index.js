3c55c3bb3bc19e9b23460c034aaa11ae0a857d06
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-05 22:26:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 /* Your code goes here */
+customerName ='bob';
+function upperCaseCustomerName(){
+  return customerName='BOB';
+}
+var bestCustomer
+function setBestCustomer(){
+  return bestCustomer='not bob';
+}
+function overWriteBestCustomer(){
+  return bestCustomer='maybe bob';
+}
+const leastFavoriteCustomer='bob'
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer='not bob';
+}
+ let favoriteCustomer=5;
+function attemptTwoFavoriteCustomers(){
+  
+   let favoriteCustomer=5;
+   
+}
